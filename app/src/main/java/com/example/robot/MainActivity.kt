6fed388163198c754b378e753f3d6b69dda1e27c
Moritz Thüning <moritz.thuening@gmail.com>
2023-09-15 23:41:55 +0200

    create new pc for offer, send failure, ice trickle

--- app/src/main/java/com/example/robot/MainActivity.kt
+++ app/src/main/java/com/example/robot/MainActivity.kt
@@ -28,16 +28,27 @@ import androidx.lifecycle.coroutineScope
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.Channel.Factory.CONFLATED
 import kotlinx.coroutines.launch
+import okhttp3.Response
 
 class MainActivity : ComponentActivity() {
+    private val ID = UUID.randomUUID().toString()
 
     private val LogTag: String = MainActivity::class.java.simpleName
 
+    private var webSocketOpen = false
+    private var webSocketQueue: String? = null
+
     private lateinit var webSocket: WebSocket
-    private lateinit var peerConnection: PeerConnection
     private lateinit var controllerId: String
+    private lateinit var eglContext: EglBase.Context
+    private lateinit var peerConnectionFactory: PeerConnectionFactory
+    private lateinit var rtcConfig: PeerConnection.RTCConfiguration
+    private lateinit var localVideoTrack: VideoTrack
+    private var peerConnection: PeerConnection? = null
+
 
     private data class Control(val throttle: Int, val steering: Int)
+
     private val controlChannel = Channel<Control>(CONFLATED)
 
     private inner class DataChannelObserver : DataChannel.Observer {
@@ -70,25 +81,21 @@ class MainActivity : ComponentActivity() {
         }
 
         override fun onIceConnectionChange(p0: PeerConnection.IceConnectionState?) {
-            Log.d(LogTag, "onIceConnectionChange")
-            if (p0 == PeerConnection.IceConnectionState.DISCONNECTED) {
-                Log.d(LogTag, "closing channel")
-                //channel?.close()
+            Log.d(LogTag, "iceconnectionstate changed to $p0")
+            if (p0 == PeerConnection.IceConnectionState.FAILED) {
+                val message = JSONObject().put("to", controllerId).put("message", "failure").toString()
+                if (webSocketOpen) {
+                    Log.d(LogTag, "send $message")
+                    webSocket.send(message)
+                } else {
+                    Log.d(LogTag, "enqueue $message")
+                    webSocketQueue = message
+                }
             }
         }
 
         override fun onIceGatheringChange(p0: PeerConnection.IceGatheringState?) {
-            if (p0 == PeerConnection.IceGatheringState.COMPLETE) {
-                Log.d(LogTag, "ice gathering complete. send offer to $controllerId")
-                val json = JSONObject()
-                json.put("to", controllerId)
-                json.put(
-                    "message",
-                    JSONObject().put("type", peerConnection.localDescription.type.canonicalForm())
-                        .put("sdp", peerConnection.localDescription.description)
-                )
-                webSocket.send(json.toString())
-            }
+            Log.d(LogTag, "onIceGatheringChange")
         }
 
         override fun onAddStream(p0: MediaStream?) {
@@ -109,6 +116,17 @@ class MainActivity : ComponentActivity() {
 
         override fun onIceCandidate(p0: IceCandidate?) {
             Log.d(LogTag, "onIceCandidate")
+            val message = JSONObject()
+            message.put("to", controllerId)
+            val candidate = JSONObject()
+            candidate.put("candidate", p0!!.sdp)
+            candidate.put(
+                "sdpMid", p0.sdpMid
+            )
+            candidate.put("sdpMLineIndex", p0.sdpMLineIndex)
+            message.put("message", candidate)
+            Log.d(LogTag, "send ice candidate $message")
+            webSocket.send(message.toString())
         }
 
         override fun onIceCandidatesRemoved(p0: Array<out IceCandidate>?) {
@@ -135,30 +153,78 @@ class MainActivity : ComponentActivity() {
         }
 
         override fun onSetFailure(p0: String?) {
-            Log.e(LogTag, "onSetFailure")
+            Log.e(LogTag, "onSetFailure $p0")
         }
     }
 
-    private inner class SignalerListener : WebSocketListener() {
+    private inner class SignalerListener(val reconnectCallback: () -> Unit) : WebSocketListener() {
         override fun onMessage(webSocket: WebSocket, text: String) {
             Log.d(LogTag, "received message from signaler $text")
             val message = JSONObject(text)
             if (message.getString("type") == "rtc") {
-                controllerId = message.getString("from")
-                val offerJson = message.getJSONObject("message")
-                val offer = SessionDescription(
-                    SessionDescription.Type.OFFER, offerJson.getString("sdp")
-                )
-                //we have remote offer, let's create answer for that
-                peerConnection.setRemoteDescription(DefaultSdpObserver(setSuccessCallback = {
-                    peerConnection.createAnswer(DefaultSdpObserver(createSuccessCallback = { p0 ->
-                        peerConnection.setLocalDescription(
-                            DefaultSdpObserver(setSuccessCallback = {
-                                Log.d(LogTag, "local description set to answer")
-                            }), p0
+                val rtcMessage = message.getJSONObject("message")
+                if (rtcMessage.has("candidate")) {
+                    peerConnection!!.addIceCandidate(
+                        IceCandidate(
+                            rtcMessage.getString("sdpMid"),
+                            rtcMessage.getInt("sdpMLineIndex"), rtcMessage.getString("candidate")
                         )
-                    }), MediaConstraints())
-                }), offer)
+                    )
+                } else if (rtcMessage.has("sdp")) {
+                    controllerId = message.getString("from")
+                    val offer = SessionDescription(
+                        SessionDescription.Type.OFFER, rtcMessage.getString("sdp")
+                    )
+                    peerConnection =
+                        peerConnectionFactory.createPeerConnection(
+                            rtcConfig,
+                            PeerConnectionObserver()
+                        )!!
+                    peerConnection!!.addTrack(localVideoTrack)
+                    peerConnection!!.setRemoteDescription(DefaultSdpObserver(setSuccessCallback = {
+                        peerConnection!!.createAnswer(DefaultSdpObserver(createSuccessCallback = { p0 ->
+                            peerConnection!!.setLocalDescription(
+                                DefaultSdpObserver(setSuccessCallback = {
+                                    val json = JSONObject()
+                                    json.put("to", controllerId)
+                                    json.put(
+                                        "message",
+                                        JSONObject().put("type", peerConnection!!.localDescription.type.canonicalForm())
+                                            .put("sdp", peerConnection!!.localDescription.description)
+                                    )
+                                    Log.d(LogTag, "send answer $json")
+                                    webSocket.send(json.toString())
+                                }), p0
+                            )
+                        }), MediaConstraints())
+                    }), offer)
+                }
+            }
+        }
+
+        override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
+            webSocketOpen = false
+            Log.e(LogTag, "websocket failure", t)
+            Thread.sleep(1000)
+            reconnectCallback()
+        }
+
+        override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
+            webSocket.close(1000, "peer requested closing");
+        }
+
+        override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
+            webSocketOpen = false
+            Log.e(LogTag, "websocket closed")
+            Thread.sleep(1000)
+            reconnectCallback()
+        }
+        override fun onOpen(webSocket: WebSocket, response: Response) {
+            webSocketOpen = true
+            Log.e(LogTag, "websocket open")
+            if (webSocketQueue != null) {
+                webSocket.send(webSocketQueue!!)
+                webSocketQueue = null
             }
         }
     }
@@ -172,7 +238,12 @@ class MainActivity : ComponentActivity() {
         Log.d(LogTag, "creates usb port $speedController")
         try {
             speedController.open(usbManager.openDevice(usbDriver.device))
-            speedController.setParameters(115200, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE)
+            speedController.setParameters(
+                115200,
+                8,
+                UsbSerialPort.STOPBITS_1,
+                UsbSerialPort.PARITY_NONE
+            )
             speedController.dtr = true
             Log.d(LogTag, "opened usb device")
             lifecycle.coroutineScope.launch {
@@ -193,8 +264,11 @@ class MainActivity : ComponentActivity() {
             Log.e(LogTag, "error opening usb device $e")
         }
 
-        val request = okhttp3.Request.Builder().url("ws://192.168.0.45:8080/connect").build()
-        webSocket = OkHttpClient().newWebSocket(request, SignalerListener())
+        val request = okhttp3.Request.Builder().url("ws://192.168.0.45:8080/connect?id=$ID").build()
+        fun connectWebSocket() {
+            webSocket = OkHttpClient().newWebSocket(request, SignalerListener(::connectWebSocket))
+        }
+        connectWebSocket()
         Log.d(LogTag, "connected to websocket")
 
 
@@ -202,21 +276,20 @@ class MainActivity : ComponentActivity() {
             PeerConnectionFactory.InitializationOptions.builder(applicationContext)
                 .createInitializationOptions()
         PeerConnectionFactory.initialize(initializationOptions)
-        val eglContext = EglBase.create().eglBaseContext
-        val peerConnectionFactory =
+        eglContext = EglBase.create().eglBaseContext
+        peerConnectionFactory =
             PeerConnectionFactory.builder().setOptions(PeerConnectionFactory.Options())
                 .setVideoEncoderFactory(DefaultVideoEncoderFactory(eglContext, true, true))
                 .setVideoDecoderFactory(DefaultVideoDecoderFactory(eglContext))
                 .createPeerConnectionFactory()
 
-        val rtcConfig = PeerConnection.RTCConfiguration(
+        rtcConfig = PeerConnection.RTCConfiguration(
             arrayListOf(
                 //PeerConnection.IceServer.builder("stun:stun.l.google.com:19302").createIceServer()
             )
         ).apply { sdpSemantics = PeerConnection.SdpSemantics.UNIFIED_PLAN }
-        peerConnection =
-            peerConnectionFactory.createPeerConnection(rtcConfig, PeerConnectionObserver())!!
-        Log.d(LogTag, "peerConnection created")
+
+        Log.d(LogTag, "peerConnectionFactory created")
 
         val cameraEnumerator = Camera2Enumerator(applicationContext)
         lateinit var capturer: VideoCapturer
@@ -233,15 +306,12 @@ class MainActivity : ComponentActivity() {
                 capturer.initialize(
                     surfaceTextureHelper, applicationContext, this.capturerObserver
                 )
-                capturer.startCapture(320, 240, 30)
+                capturer.startCapture(325, 240, 30) // 1920x1080, 1440x1080, 1088x1088, 1280x720, 1056x704, 1024x768, 960x720, 960x540, 800x450, 720x720, 720x480, 640x480, 352x288, 320x240, 256x144, 176x144
             }
         }
-        val localVideoTrack: VideoTrack by lazy {
-            peerConnectionFactory.createVideoTrack(
-                "video${UUID.randomUUID()}", videoSource
-            )
-        }
-        peerConnection.addTrack(localVideoTrack)
+        localVideoTrack = peerConnectionFactory.createVideoTrack(
+            "video${UUID.randomUUID()}", videoSource
+        )
         Log.d(LogTag, "added camera track to peerConnection")
 
         setContent {
@@ -261,7 +331,7 @@ class MainActivity : ComponentActivity() {
         super.onDestroy()
         Log.d(LogTag, "onDestroy clean up")
         webSocket.close(1000, "app closed")
-        peerConnection.close()
+        peerConnection!!.close()
     }
 }
 
